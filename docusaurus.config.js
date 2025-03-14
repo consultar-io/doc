19f903c1dd864ba1790db7a0c2dc2e175f5ce974
Gabriel Barros <gabriel.barros@expanda.com.br>
2025-03-14 12:51:28 -0300

    Update site title and logo in Docusaurus configuration to reflect new branding for Consultar.io.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -76,9 +76,9 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'My Site',
+        title: 'Consultar.io',
         logo: {
-          alt: 'My Site Logo',
+          alt: 'Consultar.io Logo',
           src: 'img/logo.svg',
         },
         items: [
