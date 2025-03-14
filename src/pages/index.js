1cbaeaafaf6f26877d2adbbfb4fcab6a49bc443f
Gabriel Barros <gabriel.barros@expanda.com.br>
2025-03-14 13:00:45 -0300

    Update documentation labels and titles to Portuguese, enhancing localization for users. Changed 'Tutorial' to 'Documentação' in multiple files including configuration, intro, and homepage components.

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,7 +20,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
+              Documentação
           </Link>
         </div>
       </div>
@@ -32,8 +32,8 @@ export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={`Documentação ${siteConfig.title}`}
+      description={`Documentação da API do ${siteConfig.title} para Desenvolvedores`}>
       <HomepageHeader />
       <main>
         <HomepageFeatures />
